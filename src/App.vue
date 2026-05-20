bc2dc3114e5a438d4d4e9036a3f61368052bc4bb
hajimeito-dltx <hitou@dl-techx-xedge.net>
2026-05-20 16:36:27 +0900

    planevue

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HomeView from './HomeView.vue'
+</script>
+
+<template>
+  <v-app>
+    <HomeView />
+  </v-app>
+</template>
---
+++ src/HomeView.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const ID = ref('')
+const Name = ref('')
+const dataList = ref([])
+
+const addData = async () => {
+  if (!ID.value || isNaN(ID.value)) {
+    console.log("IDに数値が入力されていません")
+    return
+  }
+
+  const param = {
+    ID: ID.value,
+    Name: Name.value,
+  }
+
+  const response = await axios.post(
+    'https://m3h-hajime-cashapp.jollyriver-96f47718.japaneast.azurecontainerapps.io/api/INSERT',
+    param
+  )
+
+  console.log(response.data)
+}
+
+const readData = async () => {
+  const response = await axios.get(
+    'https://m3h-hajime-cashapp.jollyriver-96f47718.japaneast.azurecontainerapps.io/api/SELECT'
+  )
+
+  console.log(response.data)
+  dataList.value = response.data.List
+}
+</script>
+
+<template>
+  <v-container>
+    <v-row class="text-h3 mb-3 mt-3" justify="center">
+      APIに接続してみよう
+    </v-row>
+
+    <v-row class="mb-3 mt-3" align="center" justify="center">
+      <v-col cols="4">
+        <v-text-field
+          class="ml-6 mr-6"
+          v-model="ID"
+          label="IDを入力"
+          placeholder="IDを入力…"
+        />
+      </v-col>
+
+      <v-col cols="6">
+        <v-text-field
+          class="ml-6 mr-6"
+          v-model="Name"
+          label="Nameを入力"
+          placeholder="Nameを入力…"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3 mt-3" align="center" justify="center">
+      入力内容　ID: {{ ID }} Name: {{ Name }}
+      <v-btn
+        @click="addData"
+        color="indigo"
+        class="ml-4"
+      >
+        DBに登録
+      </v-btn>
+    </v-row>
+
+    <v-row class="mb-3 mt-3" justify="center">
+      <v-btn @click="readData">
+        現在の登録内容を表示する
+      </v-btn>
+    </v-row>
+
+    <v-row
+      v-for="(data, index) in dataList"
+      :key="index"
+      class="mb-2"
+      justify="center"
+    >
+      ID: {{ data.ID }}, Name: {{ data.Name }}
+    </v-row>
+  </v-container>
+</template>
